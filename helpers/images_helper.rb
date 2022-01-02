ebbbbf66d1fc4c779eb69882e54c69fa5a784f6d
Jean-Baptiste Francois <jbpfran@gmail.com>
2022-01-02 16:06:02 +0100

    adding generic helpers (favicon & svg)
    new helpers to manage favicons and svg images

---
+++ helpers/images_helper.rb
@@ -0,0 +1,25 @@
+# from https://github.com/gabrielecanepa/middleman-webpack/blob/master/template/helpers/images_helper.rb
+module ImagesHelper
+  def svg_tag(file_name, attributes = {})
+    root       = Middleman::Application.root
+    file_path  = File.join(root, config.source, config.images_dir, file_name)
+    svg        = File.exist?(file_path) ? File.read(file_path) : 'SVG not found'
+    image_name = file_name.gsub(/\.svg$/, '')
+
+    add_attributes_to_opening_tag(svg, attributes)
+    # Prevent common classes (st|cl) in different icons:
+    return svg
+    svg.gsub!(/(st|cl)(?<n>\d+)/, "#{image_name}\\k<n>").html_safe
+  end
+
+  private
+
+  def add_attributes_to_opening_tag(element, attributes)
+    element.gsub!(/\s(?:height|width|x|y)="\w*"/, '')
+    new_opening_tag = '<svg '
+    attributes.each_key do |attribute|
+      new_opening_tag += "#{attribute}=\"#{attributes[attribute]}\" "
+    end
+    element.gsub!(/<svg\s/, new_opening_tag)
+  end
+end
